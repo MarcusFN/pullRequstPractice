c054e28b839733644294a10fc5dec1089826ed3d
marcusFN <marc599n@stud.kea.dk>
2023-09-08 08:58:57 +0200

    first commit

--- src/VeryBadCode.java
+++ src/VeryBadCode.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class VeryBadCode {
+public class VeryBadCode {
+
+    public class BadCode {
+        public static void main(String[] args) {
+            int x = 5;
+            int y = 10;
+
+
+            System.out.println("The sum of x and y is: " + add(x, y));
+        }
+
+        public static int add(int a, int b) {
+            int result = a + b;
+            return result;
+        }
+    }
+
 }
